43024fc3505a72112b0b34c493999f58ac5c3bf5
sunmouren <sunxuechao1024@gmail.com>
2018-09-01 23:26:44 +0800

    关闭邮箱注册，添加微博第三方登录

--- apps/writing/views.py
+++ apps/writing/views.py
@@ -110,7 +110,6 @@ class AddArticleView(LoginRequiredMixin, View):
 
     def post(self, request):
         article_form = ArticleForm(data=request.POST, files=request.FILES)
-        print(request.FILES)
         if article_form.is_valid():
             try:
                 new_article = article_form.save(commit=False)
